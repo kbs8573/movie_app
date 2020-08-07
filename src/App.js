e480feb7d93e5dde9e67c0de78b5a53fce32cc5b
김범수 선임 RPA플랫폼팀 (kbs8573@lgcns.com <78060@CORP.LGCNS.COM>
2020-08-07 15:49:16 +0900

    componet Life Cycle까지 수정한 내용 반영

--- src/App.js
+++ src/App.js
@@ -1,26 +1,88 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import PropTypes from 'prop-types';
+/*
+const bookList = [
+                    { id:0, 
+                      title:"사피엔스", 
+                      image:"https://image.yes24.com/Goods/23030284/L",
+                      rating : 4.9
+                    },
+                    { id:1, 
+                      title:"호모데우스", 
+                      image:"https://lh3.googleusercontent.com/proxy/FHqWk9bErY71ZN8TkwId2cMovwXLpHOdmb0Xi1xPm36p0zhCvwt-NhYv_S6pKsskPnWts08xzbKgT1wX2gS2pK-Sw49fmh1NeXC0303z-YIvkJrzWdGJ8Gcs-OkgYRoQ",
+                      rating : 5.0
+                    },
+                    { id:2, 
+                      title:"21세기를 위한 21가지 제언", 
+                      image:"https://img1.daumcdn.net/thumb/R720x0.q80/?scode=mtistory2&fname=http%3A%2F%2Fcfile26.uf.tistory.com%2Fimage%2F993E4A4F5BB0358A2CD392",
+                      rating : 4.5
+                    }
+                  ]                 
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+Book.propTypes = {
+  title : PropTypes.string.isRequired,
+  rating : PropTypes.number.isRequired,
+  picture : PropTypes.string.isRequired
 }
 
-export default App;
+function Book({title, picture, rating}){
+  return (
+    <div>
+      <h2>I read {title}</h2>
+      <h4>Rating : {rating}/5.0</h4>
+      <img src={picture} alt={title}></img>
+    </div>
+    );
+}
+
+
+function App() {
+
+  return (
+    <div>
+      <h1>My BookList</h1>
+      {bookList.map(book => 
+        <Book key={book.id} title={book.title} picture={book.image} rating={book.rating} />
+      )}
+    </div>    
+  );
+}
+*/
+class ClassApp extends React.Component{
+  state = {
+    count : 0,
+    isLoading : true
+  };
+
+  componentDidMount(){
+    console.log("componet Mount");
+    setTimeout(() => {
+      this.setState({isLoading : false})
+    }, 5000);
+  }
+
+  componentDidUpdate(){
+    console.log("componet Update");
+  }
+
+  add = () => {
+    console.log("Add Function");
+    console.log(this);
+    this.setState({count : 1});
+  }
+
+  render(){
+    console.log("componet Render");
+    
+    return(
+      <div>
+        <h1>{this.state.isLoading ? "Loading" : "We are Ready"}</h1>
+        <h1>count : {this.state.count}</h1>
+        <button onClick={this.add} >Add</button>
+      </div>      
+    )
+  }
+}
+
+export default ClassApp;

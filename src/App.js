2a6f8a3409dfb4e5b4deeec36ef82876e4033e0f
unknown <kbs8573@lgcns.com>
2020-09-14 10:56:06 +0900

    complete

--- src/App.js
+++ src/App.js
@@ -1,57 +1,9 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 import axios from 'axios';
-import RenderMovie from './Movies'
+import RenderMovie from './Movies';
+import './App.css';
+import './Movie.css'
 
-/*
-const bookList = [
-                    { id:0, 
-                      title:"사피엔스", 
-                      image:"https://image.yes24.com/Goods/23030284/L",
-                      rating : 4.9
-                    },
-                    { id:1, 
-                      title:"호모데우스", 
-                      image:"https://lh3.googleusercontent.com/proxy/FHqWk9bErY71ZN8TkwId2cMovwXLpHOdmb0Xi1xPm36p0zhCvwt-NhYv_S6pKsskPnWts08xzbKgT1wX2gS2pK-Sw49fmh1NeXC0303z-YIvkJrzWdGJ8Gcs-OkgYRoQ",
-                      rating : 5.0
-                    },
-                    { id:2, 
-                      title:"21세기를 위한 21가지 제언", 
-                      image:"https://img1.daumcdn.net/thumb/R720x0.q80/?scode=mtistory2&fname=http%3A%2F%2Fcfile26.uf.tistory.com%2Fimage%2F993E4A4F5BB0358A2CD392",
-                      rating : 4.5
-                    }
-                  ]                 
-
-
-Book.propTypes = {
-  title : PropTypes.string.isRequired,
-  rating : PropTypes.number.isRequired,
-  picture : PropTypes.string.isRequired
-}
-
-function Book({title, picture, rating}){
-  return (
-    <div>
-      <h2>I read {title}</h2>
-      <h4>Rating : {rating}/5.0</h4>
-      <img src={picture} alt={title}></img>
-    </div>
-    );
-}
-
-
-function App() {
-
-  return (
-    <div>
-      <h1>My BookList</h1>
-      {bookList.map(book => 
-        <Book key={book.id} title={book.title} picture={book.image} rating={book.rating} />
-      )}
-    </div>    
-  );
-}
-*/
 class App extends React.Component{
   state = {
     isLoading : true,
@@ -61,7 +13,7 @@ class App extends React.Component{
   getMovies = async() => {
     console.log('getMovies');
     const result = await axios.get('https://yts-proxy.now.sh/list_movies.json');
-    //console.log(result.data.data.movies);
+      console.log(result.data.data.movies);
     this.setState({isLoading:false, movies : result.data.data.movies})
   }
 
@@ -83,8 +35,8 @@ class App extends React.Component{
     console.log("componet Render");
 
     return(
-      <div>
-        {this.state.isLoading ? "Loading..." : this.state.movies.map(movie => <RenderMovie key={movie.id} id={movie.id} title={movie.title} url={movie.medium_cover_image} />)}
+      <div className="movies">
+        {this.state.isLoading ? "Loading..." : this.state.movies.map(movie => <RenderMovie key={movie.id} title={movie.title} url={movie.large_cover_image} rating={movie.rating} summary={movie.summary} genres={movie.genres} year={movie.year} />)}
       </div>      
     )
   }
--- src/Movies.js
+++ src/Movies.js
@@ -1,10 +1,23 @@
-import React from 'react'
+import React from 'react';
+import './Movie.css';
+import PropTypes from 'prop-types';
 
-function RenderMovie({ id, title, url }){
+RenderMovie.propTypes = {
+    title : PropTypes.string.isRequired
+}
+
+
+
+function RenderMovie({ title, url, rating, summary, genres, year }){
     return (
-        <div>
-            <h4>{ title }</h4>
-            <img src={url}></img>
+        <div className="movie">
+            <img src={url} alt={title} title={title}></img>
+            <a className="title">{ title }</a>
+            <a className="year">{year}</a>
+            <p>{summary.slice(0,140)}...</p>
+            <ul className="genres">
+                {genres.map((genre, itemNo) => <li key={itemNo}>{genre}</li> )}
+            </ul>
         </div>
     );
 }
